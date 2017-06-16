989a68f0702f685b5eadf9732aedca1a59b8fe72
Kaplaugher <kaplaugher94@gmail.com>
2017-06-16 10:48:45 -0400

    finished

--- starbase-router/src/main.js
+++ starbase-router/src/main.js
@@ -4,12 +4,18 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 import App from './App.vue'
+import Data from './components/Data.vue'
 
-const routers = [
-  { path: '/data/:type'}
+const routes = [
+  { path: '/data/:type', component: Data }
 ]
+
+const router = new VueRouter ({
+  routes
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
